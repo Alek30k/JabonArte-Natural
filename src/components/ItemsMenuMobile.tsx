86028376ac14f459ad732abba32b4b9974264548
Alek30k <alemank23k@gmail.com>
2025-05-20 23:19:58 -0300

    last commit

--- src/components/ItemsMenuMobile.tsx
+++ src/components/ItemsMenuMobile.tsx
@@ -1,16 +1,24 @@
+import { Menu } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import Link from "next/link";
 
 const ItemsMenuMobile = () => {
   return (
     <div className="">
       <Popover>
         <PopoverTrigger>
-          <p>Menu</p>
+          <Menu />
         </PopoverTrigger>
         <PopoverContent>
-          <p>Café Molido</p>
-          <p>Café Grano</p>
-          <p>Café Capsula</p>
+          <Link href="/categories/cafe-molido" className="block">
+            Café Molido
+          </Link>
+          <Link href="/categories/cafe-molido" className="block">
+            Café Grano
+          </Link>
+          <Link href="/categories/cafe-molido" className="block">
+            Café Capsulas
+          </Link>
         </PopoverContent>
       </Popover>
     </div>
