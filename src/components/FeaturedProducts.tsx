95280635e4171553329dea2259570bfdef3e477d
Alek30k <alemank23k@gmail.com>
2025-05-21 14:43:28 -0300

    last commit

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,5 +1,11 @@
 "use client";
+
+import { useGetFeaturedProducts } from "@/api/useGetFeaturedProducts";
+
 const FeaturedProducts = () => {
+  const { loading, result } = useGetFeaturedProducts();
+
+  console.log(result);
   return (
     <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
       <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
