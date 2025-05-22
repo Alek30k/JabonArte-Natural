6cf12e9c6519e7242ae82deaaed7ea58a8248fc8
Alek30k <alemank23k@gmail.com>
2025-05-22 15:06:31 -0300

    last commit

--- src/app/(routes)/category/[categorySlug]/page.tsx
+++ src/app/(routes)/category/[categorySlug]/page.tsx
@@ -1,20 +1,36 @@
 "use client";
 
 import { useGetCategoryProduct } from "@/api/getCategoryProduct";
+import { Separator } from "@/components/ui/separator";
 import { ResponseType } from "@/types/response";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Page() {
   const params = useParams();
-  const categorySlug = params.categorySlug as string
-  const {result loading, error} :ResponseType = useGetCategoryProduct(categorySlug)
-
+  const categorySlug = params.categorySlug as string;
+  const { result, loading, error }: ResponseType =
+    useGetCategoryProduct(categorySlug);
+  const router = useRouter();
 
   console.log(result);
 
-   if (loading) return <div className="p-8 text-center">Cargando productos...</div>
-  if (error) return <div className="p-8 text-center text-red-500">Error: {error}</div>
-  if (!result || result.length === 0) return <div className="p-8 text-center">No hay productos en esta categoría</div>
+  if (loading)
+    return <div className="p-8 text-center">Cargando productos...</div>;
+  if (error)
+    return <div className="p-8 text-center text-red-500">Error: {error}</div>;
+  if (!result || result.length === 0)
+    return (
+      <div className="p-8 text-center">No hay productos en esta categoría</div>
+    );
 
-  return <>From page category</>;
+  return (
+    <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
+      {result !== null && !loading && (
+        <h1 className="text-3xl font-medium">
+          Café {result[0].category.categoryName}
+        </h1>
+      )}
+      <Separator />
+    </div>
+  );
 }
