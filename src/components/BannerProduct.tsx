c79dece33728a91040a430f56109a1daad39f2de
Alek30k <alemank23k@gmail.com>
2025-05-22 13:18:28 -0300

    last commit

--- src/components/BannerProduct.tsx
+++ src/components/BannerProduct.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
 const BannerProduct = () => {
-  return <div className="">BannerProduct</div>;
+  return (
+    <>
+      <div className="mt-4 text-center">
+        <p>Sumérgete en una experiencia única</p>
+        <h4 className="mt-2 text-5xl font-extrabold uppercase">
+          CaféExquisito
+        </h4>
+        <p className="my-2 text-lg">Despierta tus sentidos con cada sorbo</p>
+        <Link href="#" className={buttonVariants()}>
+          Comprar
+        </Link>
+      </div>
+      <div className='h-[350px] bg-cover lg:h-[400px] bg-[url("/slider.png")] bg-center mt-5'></div>
+    </>
+  );
 };
 
 export default BannerProduct;
