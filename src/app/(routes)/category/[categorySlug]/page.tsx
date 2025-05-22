c2167a5a3cec41f9bc2f536ae812598c888b5482
Alek30k <alemank23k@gmail.com>
2025-05-22 14:25:51 -0300

    last commit

--- src/app/(routes)/category/[categorySlug]/page.tsx
+++ src/app/(routes)/category/[categorySlug]/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { useGetCategoryProduct } from "@/api/getCategoryProduct";
+import { ResponseType } from "@/types/response";
+import { useParams } from "next/navigation";
+
 export default function Page() {
+  const params = useParams();
+  const categorySlug = params.categorySlug as string
+  const {result loading, error} :ResponseType = useGetCategoryProduct(categorySlug)
+
+
+  console.log(result);
+
+   if (loading) return <div className="p-8 text-center">Cargando productos...</div>
+  if (error) return <div className="p-8 text-center text-red-500">Error: {error}</div>
+  if (!result || result.length === 0) return <div className="p-8 text-center">No hay productos en esta categoría</div>
+
   return <>From page category</>;
 }
