28f18c9e89b29a2a2904bab25505eb769ed029f4
Alek30k <alemank23k@gmail.com>
2025-05-22 16:27:31 -0300

    last commit

--- src/app/(routes)/category/[categorySlug]/components/FilterOrigin.tsx
+++ src/app/(routes)/category/[categorySlug]/components/FilterOrigin.tsx
@@ -1,17 +1,25 @@
 import { useGetProductField } from "@/api/getProductField";
-import { RadioGroup } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { FilterTypes } from "@/types/filter";
 
 const FilterOrigin = () => {
-  const { result, loading } = useGetProductField();
-
-  console.log(result);
+  const { result, loading }: FilterTypes = useGetProductField();
 
   return (
     <div className="my-5">
       <p className="mb-3 font-bold">Origin</p>
       {loading && result === null && <p>Cargando origen...</p>}
 
-      <RadioGroup></RadioGroup>
+      <RadioGroup>
+        {result !== null &&
+          result.schema.attributes.origin.enum.map((origin: string) => (
+            <div className="flex items-center space-x-2" key={origin}>
+              <RadioGroupItem value={origin} id={origin} />
+              <Label htmlFor={origin}>{origin}</Label>
+            </div>
+          ))}
+      </RadioGroup>
     </div>
   );
 };
