99b14a769c6b376265e4c923eae0009f543ac50a
Alek30k <alemank23k@gmail.com>
2025-05-21 22:38:57 -0300

    add banner discount

--- src/components/BannerDiscount.tsx
+++ src/components/BannerDiscount.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
 const BannerDiscount = () => {
-  return <div className="">BannerDiscount</div>;
+  return (
+    <div className="p-5 sm:p-20 text-center">
+      <h2 className="uppercase font-black text-2xl text-primary">
+        Consigue hasta un -20%
+      </h2>
+      <h3 className="mt-3 font-semibold">
+        -10% al gastar 10.000$ o -15% al gastar 15.000$. Usa el código
+        JABONARTENATURAL
+      </h3>
+      <div className="max-w-md mx-auto sm:flex justify-center gap-8 mt-5 ">
+        <Link href="#" className={buttonVariants()}>
+          Comprar
+        </Link>
+        <Link href="#" className={buttonVariants({ variant: "outline" })}>
+          Más información
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default BannerDiscount;
