96a1dbfe01dc7b0d71c85c948539f479a211fee7
Alek30k <alemank23k@gmail.com>
2025-05-22 22:42:30 -0300

    last commit

--- src/app/(routes)/product/[productSlug]/page.tsx
+++ src/app/(routes)/product/[productSlug]/page.tsx
@@ -4,6 +4,7 @@ import { useGetProductBySlug } from "@/api/getProductBySlug";
 import { ResponseType } from "@/types/response";
 import { useParams } from "next/navigation";
 import SkeletonProduct from "./components/SkeletonProduct";
+import CarouselProduct from "./components/CarouselProduct";
 
 export default function Page() {
   const params = useParams();
@@ -18,7 +19,7 @@ export default function Page() {
     <div className="max-w-6xl py-4 mx-auto sm:py-32 sm:px-24">
       <div className="grid sm:grid-cols-2">
         <div className="">
-          <p>Carousel Product</p>
+          <CarouselProduct images={result[0].images} />
         </div>
         <div className="sm:px-12">
           <p>Info del producto</p>
