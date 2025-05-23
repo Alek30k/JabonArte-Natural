777acf70f5a1ee203fd632a43fa0f30814b3e7d4
Alek30k <alemank23k@gmail.com>
2025-05-23 14:32:41 -0300

    last commit

---
+++ src/app/(routes)/loved-products/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div className="max-w-4xl py-4 mx-auto sm:py-32 sm:px-24">
+      <h1 className="sm:text-2xl">Productos que te gustan</h1>
+      <div className="">Loved products</div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/hooks/UseLovedProducts.tsx
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import { ProductType } from "@/types/product";
+import { toast } from "sonner";
+
+interface UseLovedProductsType {
+  lovedItems: ProductType[];
+  addLovedItem: (data: ProductType) => void;
+  removeLovedItem: (id: number) => void;
+}
+export const UseLovedProducts = create(
+  persist<UseLovedProductsType>(
+    (set, get) => ({
+      lovedItems: [],
+      addLovedItem: (data: ProductType) => {
+        const currentLovedItems = get().lovedItems;
+        const existingItem = currentLovedItems.find(
+          (item) => item.id === data.id
+        );
+
+        if (existingItem) {
+          toast.error("El producto ya existe en la lista 💛 ");
+          return;
+        }
+
+        set({
+          lovedItems: [...get().lovedItems, data],
+        });
+        toast.success("Producto agregado a la lista 💚");
+      },
+
+      removeLovedItem: (id: number) => {
+        set({
+          lovedItems: [...get().lovedItems.filter((item) => item.id !== id)],
+        });
+        toast("Producto eliminado de la lista 💔");
+      },
+    }),
+    {
+      name: "loved-products-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
