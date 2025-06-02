a0adf37bf975dcccb65fabdb505f7559d7224823
Alek30k <alemank23k@gmail.com>
2025-06-01 22:58:58 -0300

    last commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,37 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isPublicRoute = createRouteMatcher([
-//   "/",
-//   "/category/:path*",
-//   "/products/:path*",
-//   "/privacy",
-//   "/product",
-//   "/category",
-//   "/sign-in",
-//   "/sign-up",
-// ]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/category/:path*",
+  "/products/:path*",
+  "/privacy",
+  "/product",
+  "/category",
+  "/sign-in",
+  "/sign-up",
+]);
 
-// export default clerkMiddleware(async (auth, request) => {
-//   if (!isPublicRoute(request)) {
-//     const { redirectToSignIn } = await auth();
-//     return redirectToSignIn({ returnBackUrl: request.url });
-//   }
-// });
-
-// export const config = {
-//   matcher: [
-//     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:jpg|jpeg|png|svg|ico|webp)$|api/webhook).*)",
-//     "/",
-//     "/(api|trpc)(.*)",
-//   ],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware(async (auth) => {
-  await auth.protect();
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn({ returnBackUrl: request.url });
+  }
 });
 
 export const config = {
-  matcher: ["/profile(.*)", "/orders(.*)"], // Solo proteger estas rutas
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:jpg|jpeg|png|svg|ico|webp)$|api/webhook).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware(async (auth) => {
+//   await auth.protect();
+// });
+
+// export const config = {
+//   matcher: ["/profile(.*)", "/orders(.*)"], // Solo proteger estas rutas
+// };
